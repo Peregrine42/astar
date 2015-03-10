8adc7354c5e4ed746fb2e245e0f9218d3263bfb7
Duncan Fermor <duncan@elitemail.org>
2015-03-10 22:50:40 +0000

    breadth first search complete

--- breadth-first.rb
+++ breadth-first.rb
@@ -1,14 +1,29 @@
-def breadth_first graph
-  frontier = Queue.new
-  frontier.put start
+class Graph
+  def initialize nodes, connections
+    @nodes = nodes
+    @connections = connections
+  end
+
+  def neighbours node
+    return @connections
+      .select { |connection| connection.include? node }
+      .map    { |connection| connection.find { |n| n != node } }
+      .uniq
+  end
+
+end
+
+def breadth_first graph, start
+  frontier = []
+  frontier.unshift start
   visited = {}
   visited[start] = true
 
   while !frontier.empty? do
-    current = frontier.get
+    current = frontier.pop
     graph.neighbours(current).each do |the_next|
       if !visited.include? the_next
-        frontier.put the_next
+        frontier.unshift the_next
         visited[the_next] = true
       end
     end
--- breadth_first_spec.rb
+++ breadth_first_spec.rb
@@ -1,13 +1,52 @@
+require './breadth-first'
+
 describe 'graph.neighbours' do
   it 'returns the neighbours of a node in a graph' do
+    a = double :a
+    b = double :b
+    c = double :c
+
+    nodes = [a, b, c]
+    links = [[a, b], [a, c]]
     graph = Graph.new(nodes, links)
-    expect(graph.neighbours(a)).to eq [b, c, d]
+    expect(graph.neighbours(a)).to eq [b, c]
+  end
+
+  it 'returns the neighbours of a node in a graph with loops' do
+    a = double :a
+    b = double :b
+    c = double :c
+
+    nodes = [a, b, c]
+    links = [[a, b], [a, c], [c, b]]
+    graph = Graph.new(nodes, links)
+    expect(graph.neighbours(a)).to eq [b, c]
+  end
+
+  it 'returns neighbours in a graph with connections in both directions' do
+    a = double :a
+    b = double :b
+    c = double :c
+
+    nodes = [a, b, c]
+    links = [[a, b], [a, c], [c, b], [c, a]]
+    graph = Graph.new(nodes, links)
+    expect(graph.neighbours(a)).to eq [b, c]
   end
 end
 
 describe 'breadth first' do
 
-  xit "visits all the nodes in a graph" do
+  it "visits all the nodes in a graph" do
+    a = double :a
+    b = double :b
+    c = double :c
+
+    nodes = [a, b, c]
+    links = [[a, b], [a, c], [c, b]]
+    graph = Graph.new(nodes, links)
+
+    expect(breadth_first(graph, a).keys).to include a, b, c
   end
 
 end
